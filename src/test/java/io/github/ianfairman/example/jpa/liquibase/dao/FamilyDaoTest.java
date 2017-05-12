ceb5832b6757a068ea606f0938373c60c5e5d7d6
Ian Fairman <ian.fairman@kymab.com>
2017-05-12 13:31:52 +0100

    Remove "is" from assertions. Make a "test" a proper test.

--- src/test/java/io/github/ianfairman/example/jpa/liquibase/dao/FamilyDaoTest.java
+++ src/test/java/io/github/ianfairman/example/jpa/liquibase/dao/FamilyDaoTest.java
@@ -1,27 +1,25 @@
 package io.github.ianfairman.example.jpa.liquibase.dao;
 
-import io.github.ianfairman.example.jpa.liquibase.dao.FamilyDao;
+import io.github.ianfairman.example.jpa.liquibase.entity.Family;
+import static io.github.ianfairman.example.jpa.liquibase.value.FamilyId.familyId;
+import static io.github.ianfairman.example.jpa.liquibase.value.LastName.lastName;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import io.github.ianfairman.example.jpa.liquibase.entity.Family;
-import static io.github.ianfairman.example.jpa.liquibase.value.FamilyId.familyId;
-import static io.github.ianfairman.example.jpa.liquibase.value.LastName.lastName;
 
 public class FamilyDaoTest {
 
     private static FamilyDao familyDao;
     private static EntityManager entityManager;
-    
+
     @BeforeClass
     public static void configureDao() {
-        EntityManagerFactory emf=Persistence.createEntityManagerFactory("people");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("people");
         entityManager = emf.createEntityManager();
         familyDao = new FamilyDao(entityManager);
     }
@@ -30,44 +28,45 @@ public class FamilyDaoTest {
     public void shouldRetrieveAllFamiliesForFindAll() {
         // When
         List<Family> families = familyDao.findAll();
-        
+
         // Then
-        assertThat(families.get(0).getId(), is(equalTo(familyId(0))));
-        assertThat(families.get(0).getLastName(), is(equalTo(lastName("Rubble"))));
-        assertThat(families.get(1).getId(), is(equalTo(familyId(1))));
-        assertThat(families.get(1).getLastName(), is(equalTo(lastName("Simpson"))));
-        assertThat(families.get(2).getId(), is(equalTo(familyId(2))));
-        assertThat(families.get(2).getLastName(), is(equalTo(lastName("Flintstone"))));
+        assertThat(families.get(0).getId(), equalTo(familyId(0)));
+        assertThat(families.get(0).getLastName(), equalTo(lastName("Rubble")));
+        assertThat(families.get(1).getId(), equalTo(familyId(1)));
+        assertThat(families.get(1).getLastName(), equalTo(lastName("Simpson")));
+        assertThat(families.get(2).getId(), equalTo(familyId(2)));
+        assertThat(families.get(2).getLastName(), equalTo(lastName("Flintstone")));
     }
-    
+
     @Test
     public void shouldRetrieveSimpsonById1() {
         // When
         Family family = familyDao.findById(1);
-        
+
         // Then
-        assertThat(family.getId(), is(equalTo(familyId(1))));
-        assertThat(family.getLastName(), is(equalTo(lastName("Simpson"))));
+        assertThat(family.getId(), equalTo(familyId(1)));
+        assertThat(family.getLastName(), equalTo(lastName("Simpson")));
     }
-    
+
     @Test
     public void shouldRetrieveSimpsonByName() {
         // When
         Family family = familyDao.findByLastName(lastName("Simpson")).get(0);
-        
+
         // Then
-        assertThat(family.getId(), is(equalTo(familyId(1))));
-        assertThat(family.getLastName(), is(equalTo(lastName("Simpson"))));
+        assertThat(family.getId(), equalTo(familyId(1)));
+        assertThat(family.getLastName(), equalTo(lastName("Simpson")));
     }
-    
+
     @Test
     public void shouldCreateANewFamily() {
         // When
         entityManager.getTransaction().begin();
         Family family = familyDao.create(lastName("Bear"));
         entityManager.getTransaction().commit();
-        
+
         // Then
-        System.out.println(family);
+        List<Family> families = familyDao.findByLastName(lastName("Bear"));
+        assertThat(families.get(0), equalTo(family));
     }
 }
--- src/test/java/io/github/ianfairman/example/jpa/liquibase/dao/IndividualDaoTest.java
+++ src/test/java/io/github/ianfairman/example/jpa/liquibase/dao/IndividualDaoTest.java
@@ -1,26 +1,24 @@
 package io.github.ianfairman.example.jpa.liquibase.dao;
 
-import io.github.ianfairman.example.jpa.liquibase.dao.IndividualDao;
+import io.github.ianfairman.example.jpa.liquibase.entity.Individual;
+import static io.github.ianfairman.example.jpa.liquibase.value.FirstName.firstName;
+import static io.github.ianfairman.example.jpa.liquibase.value.IndividualId.individualId;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static io.github.ianfairman.example.jpa.liquibase.value.FirstName.firstName;
-import io.github.ianfairman.example.jpa.liquibase.entity.Individual;
-import static io.github.ianfairman.example.jpa.liquibase.value.IndividualId.individualId;
 
 public class IndividualDaoTest {
-    
+
     private static IndividualDao individualDao;
-    
+
     @BeforeClass
     public static void configureDao() {
-        EntityManagerFactory emf=Persistence.createEntityManagerFactory("people");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("people");
         EntityManager em = emf.createEntityManager();
         individualDao = new IndividualDao(em);
     }
@@ -29,33 +27,33 @@ public class IndividualDaoTest {
     public void shouldRetrieveAllindividualsForFindAll() {
         // When
         List<Individual> individuals = individualDao.findAll();
-        
+
         // Then
-        assertThat(individuals.get(0).getId(), is(equalTo(individualId(0))));
-        assertThat(individuals.get(0).getFirstName(), is(equalTo(firstName("Bart"))));
-        assertThat(individuals.get(1).getId(), is(equalTo(individualId(1))));
-        assertThat(individuals.get(1).getFirstName(), is(equalTo(firstName("Homer"))));
-        assertThat(individuals.get(2).getId(), is(equalTo(individualId(2))));
-        assertThat(individuals.get(2).getFirstName(), is(equalTo(firstName("Marge"))));
+        assertThat(individuals.get(0).getId(), equalTo(individualId(0)));
+        assertThat(individuals.get(0).getFirstName(), equalTo(firstName("Bart")));
+        assertThat(individuals.get(1).getId(), equalTo(individualId(1)));
+        assertThat(individuals.get(1).getFirstName(), equalTo(firstName("Homer")));
+        assertThat(individuals.get(2).getId(), equalTo(individualId(2)));
+        assertThat(individuals.get(2).getFirstName(), equalTo(firstName("Marge")));
     }
-    
+
     @Test
     public void shouldRetrieveHomerById1() {
         // When
         Individual individual = individualDao.findById(1);
-        
+
         // Then
-        assertThat(individual.getId(), is(equalTo(individualId(1))));
-        assertThat(individual.getFirstName(), is(equalTo(firstName("Homer"))));
+        assertThat(individual.getId(), equalTo(individualId(1)));
+        assertThat(individual.getFirstName(), equalTo(firstName("Homer")));
     }
-    
+
     @Test
     public void shouldRetrieveHomerByName() {
         // When
-        Individual individual =  individualDao.findByFirstName("Homer").get(0);
-        
+        Individual individual = individualDao.findByFirstName("Homer").get(0);
+
         // Then
-        assertThat(individual.getId(), is(equalTo(individualId(1))));
-        assertThat(individual.getFirstName(), is(equalTo(firstName("Homer"))));
+        assertThat(individual.getId(), equalTo(individualId(1)));
+        assertThat(individual.getFirstName(), equalTo(firstName("Homer")));
     }
 }
